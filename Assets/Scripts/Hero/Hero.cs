6c9fd973add8df67134000bb09ee5bd9930788c0
Mikhail Tolmachev <mikhaeltolmachev@gmail.com>
2026-01-13 01:16:43 +0300

    InputManager added

---
+++ Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+namespace Hero
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        
+        private float direction;
+      
+        public void SetDirection(float dir)
+        {
+            this.direction = dir;
+        }
+
+        public void SaySomething()
+        {
+            Debug.Log("SaySomething");
+        }
+
+        private void Update()
+        {
+            if (direction != 0)
+            {
+                var delta = direction * speed * Time.deltaTime;
+                var newXPosition = transform.position.x + delta;
+                transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+            }
+                
+        }
+    }
+}
---
+++ Assets/Scripts/Hero/HeroInputReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+namespace Hero
+{
+   public class HeroInputReader : MonoBehaviour
+   {
+      private Hero hero;
+      
+      private void Awake()
+      {
+         hero = GetComponent<Hero>();
+      }
+
+      public void OnHorizontalMovement(InputAction.CallbackContext context)
+      {
+         var direction = context.ReadValue<float>();
+         hero.SetDirection(direction);
+      }
+
+      public void OnSaySomething(InputAction.CallbackContext context)
+      {
+         if(context.canceled)
+            hero.SaySomething();
+      }
+   }
+}
