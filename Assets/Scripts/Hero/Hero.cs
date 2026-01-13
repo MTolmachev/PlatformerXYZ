9274f56c56cbdb6119711ee72a1d79278109f384
Mikhail Tolmachev <mikhaeltolmachev@gmail.com>
2026-01-13 14:09:15 +0300

    Climbing added

--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -7,12 +7,13 @@ namespace Hero
     public class Hero : MonoBehaviour
     {
         [SerializeField] private float speed;
+        [SerializeField] private float climbSpeed;
         [SerializeField] private float jumpForce;
         
         private Vector2 direction;
         private Rigidbody2D rb;
         
-        [SerializeField] private bool canClimb = false;
+        [SerializeField] private bool onChain = false;
         [SerializeField] private bool onFloor = true;
 
         private void Awake()
@@ -21,20 +22,16 @@ namespace Hero
         }
         private void Update()
         {
-            if (direction.x != 0 && onFloor)
+            if (direction.x != 0)
             {
                 var delta = direction.x * speed * Time.deltaTime;
                 var newXPosition = transform.position.x + delta;
                 transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
             }
-            // Небольшая симуляция лазания по лестнице
-            if (direction.y != 0 && canClimb)
+            if (direction.y != 0 && onChain)
             {
-                rb.simulated = false;
-                onFloor = false;
-                var delta = direction.y * speed * Time.deltaTime;
-                var newYPosition = transform.position.y + delta;
-                transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+                var vertical = direction.y;
+                rb.velocity = new Vector2(0f, vertical * climbSpeed);
             }
                 
         }
@@ -42,11 +39,37 @@ namespace Hero
         public void Jump()
         {
             rb.velocity = new Vector2(direction.x, jumpForce);
+            
+            if (onChain)
+            {
+                onChain = false;
+                rb.gravityScale = 1;
+                rb.velocity = new Vector2(jumpForce, jumpForce);
+            }
         }
 
-        public void SetDirection(Vector2  dir)
+        public void SetDirection(Vector2 dir)
         {
             this.direction = dir;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Chain"))
+            { 
+               onChain = true;
+               rb.gravityScale = 0;
+               rb.velocity = Vector2.zero;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Chain"))
+            {
+                onChain = false;
+                rb.gravityScale = 1;
+            }
         }
 
         public void SaySomething()
