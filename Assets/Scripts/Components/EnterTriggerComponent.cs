bd3c7238c61dfaf3493deaa7dcb7b582f2fe4450
Mikhail Tolmachev <mikhaeltolmachev@gmail.com>
2026-01-16 21:33:49 +0300

    Level reset added

---
+++ Assets/Scripts/Components/EnterTriggerComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Components
+{
+    public class EnterTriggerComponent : MonoBehaviour
+    {
+        [SerializeField] private string tag;
+        [SerializeField] private UnityEvent action;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(tag))
+            {
+                action?.Invoke();
+            }
+        }
+    }
+}
